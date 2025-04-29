cc64b9cef39071a32ad203fa7aee02d73b39622a
anant23O02 <anant.sharma@qsstechnosoft.com>
2025-04-29 17:37:48 +0530

    added navbar

--- client/src/app/pages/page.tsx
+++ client/src/app/pages/page.tsx
@@ -1,11 +1,13 @@
 
 import Navbar from "@/components/navbar/navbar";
+import Sidebar from "@/components/Sidebar/sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
      <Navbar/>
+     <Sidebar></Sidebar>
     </>
   );
 }
