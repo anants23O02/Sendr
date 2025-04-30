88d1b68171894d839bb56cf7584457a4c3ad51d2
Parlakshit Tyagi <parlakshit.tyagi@qsstechnosoft.com>
2025-04-30 16:26:09 +0530

    created login and signup page

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -3,6 +3,9 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
 
 export function LoginForm({
   className,
@@ -58,32 +61,23 @@ export function LoginForm({
                   type="button"
                   className="w-full flex items-center gap-2"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-5 h-5"
-                  >
-                    <path d="M22.675 0h-21.35C.597 0 0 .597 0 1.326v21.348C0 23.403.597 24 1.326 24h11.494v-9.294H9.691V11.01h3.129V8.414c0-3.1 1.894-4.788 4.659-4.788 1.325 0 2.464.099 2.797.143v3.243l-1.918.001c-1.504 0-1.796.715-1.796 1.763v2.311h3.587l-.467 3.696h-3.12V24h6.116C23.403 24 24 23.403 24 22.674V1.326C24 .597 23.403 0 22.675 0z" />
-                  </svg>
+                  <FaFacebook />
                   <span className="sr-only">Login with Facebook</span>
                 </Button>
 
                 <Button variant="outline" type="button" className="w-full">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
+                  <FcGoogle />
                   <span className="sr-only">Login with Google</span>
                 </Button>
               </div>
               <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
+                <Link
+                  href="/pages/SignUp"
+                  className="underline underline-offset-4"
+                >
                   Sign up
-                </a>
+                </Link>
               </div>
             </div>
           </form>
---
+++ client/src/components/signUpForm/signUpForm.tsx
@@ -0,0 +1,147 @@
+'use client'
+
+import Link from "next/link";
+import clsx from "clsx";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+import { FaRegEye } from "react-icons/fa";
+
+export function SignUpForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const [show, setShow] = useState(false);
+
+  function handleShow() {
+    setShow((prev)=>!prev);
+  }
+  return (
+    <div className={clsx("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">
+                  {" "}
+                  Create an Sendrr Account
+                </h1>
+                <p className="text-muted-foreground text-balance mt-3 text-sm">
+                  Let's get started. Fill in the details below to create your
+                  account
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Name</Label>
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="Alex Johns"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Contact No.</Label>
+                <Input
+                  id="contact"
+                  type="number"
+                  min={0}
+                  max={9999999999}
+                  placeholder="0129102198"
+                  required
+                />
+              </div>
+              <div className="grid gap-3 relative">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+                {show === false ? (
+                  <FaRegEyeSlash
+                    onClick={handleShow}
+                    className="absolute top-9 right-4 cursor-pointer"
+                  />
+                ) : (
+                  <FaRegEye onClick={handleShow} className="absolute top-9 right-4 cursor-pointer" />
+                )}
+              </div>
+              <div className="grid gap-3 relative">
+                <div className="flex items-center">
+                  <Label htmlFor="confirm password">Confirm Password</Label>
+                </div>
+                <Input id="confirm password" type="password" required />
+                {show === false ? (
+                  <FaRegEyeSlash
+                    onClick={handleShow}
+                    className="absolute top-9 right-4  cursor-pointer"
+                  />
+                ) : (
+                  <FaRegEye onClick={handleShow} className="absolute top-9 right-4 cursor-pointer" />
+                )}
+              </div>
+              <Button
+                type="submit"
+                className="w-full bg-black text-white font-medium"
+              >
+                Sign Up
+              </Button>
+              <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                <span className="bg-card text-muted-foreground relative z-10 px-2">
+                  Or continue with
+                </span>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <Button
+                  variant="outline"
+                  type="button"
+                  className="w-full flex items-center gap-2"
+                >
+                  <FaFacebook />
+                  <span className="sr-only">Login with Facebook</span>
+                </Button>
+
+                <Button variant="outline" type="button" className="w-full">
+                  <FcGoogle />
+                  <span className="sr-only">Login with Google</span>
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                Do you have an account?{" "}
+                <Link
+                  href="/pages/Login"
+                  className="underline underline-offset-4"
+                >
+                  Login
+                </Link>
+              </div>
+            </div>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/assests/premium_photo-1677252438450-b779a923b0f6.jpg"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
