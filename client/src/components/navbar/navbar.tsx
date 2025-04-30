d7d67d32510c0361c5ce40b017609fbeee003b27
Vinay Sharma <vinay.sharma@qsstechnosoft.com>
2025-04-30 10:58:12 +0530

    adding the nav

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
 import { Search } from "lucide-react";
 import { SearchBarProps } from "./navbarInterface";
+import Image from "next/image";
+import logo  from "../../../public/logo.svg"
 
 
 const Navbar: FC<SearchBarProps> = ({ placeholder = "Search in Public Relations" }) => {
   return (
-    <div className="bg-[#e6eaee] rounded-md p-1">
-      <div className="flex items-center bg-[#1b1b1b] rounded-md px-4 py-4">
+    <div className="bg-[#2e2e2e] rounded-md flex items-center justify-between w-full px-4 py-2">
+    <div>
+      <Image src={logo} alt="Logo" width={100} height={50} className="h-8 w-auto" />
+    </div>
+      <div className="flex items-center bg-[#808080] rounded-md px-4 py-4">
         <Search className="text-white opacity-70 mr-2 h-4 w-4" />
         <input
           type="text"
